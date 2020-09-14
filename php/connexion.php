665f4a2d6a408903f46eb36d1706b4d06ba76d48
yannrdg <yann.radigon@gmail.com>
2020-09-14 14:28:57 +0200

    Création des inputs
    J'ai créé 2 inputs permettant d'inscrire le login et le mot de passe de la personne afin qu'il puisse se connecter.

---
+++ php/connexion.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form method="post">
+        <input type="text" id="login" placeholder="login" name="login">
+        <input type="text" id="mdp" name="mdp" placeholder="mdp">
+    </form>
+</body>
+</html>
