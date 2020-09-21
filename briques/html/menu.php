5d63cfcdcff1b15c07dee482d1f3f88b18a93127
yannrdg <yann.radigon@gmail.com>
2020-09-21 15:26:15 +0200

    Affichage en fonction de l'état de connexion
    J'ai ajouté une fonction if dans le menu.php qui permet de gérer les liens à afficher en fonction de l'état de la connexion.
    
    Grace à la varaible $_SESSION[] je regarde si l'utilisateur est connecté. S'il est connecté j'affiche le lien déconnexion et j'enlève le lien connexion et inscription. Et s'il n'est pas connecté je fais l'inverse.

--- briques/html/menu.php
+++ briques/html/menu.php
@@ -1,8 +1,21 @@
 <nav>
     <ul>
         <li><a href="accueil.php">Accueil</a></li>
-        <li><a href="connexion.php">Connexion</a></li>
-        <li><a href="inscription.php">Inscription</a></li>
+        <?php
+            if($_SESSION['mail'])
+            {
+        ?>
         <li><a href="deconnexion.php">Déconnexion</a></li>
+        <?php
+            }
+            else
+            {
+        ?>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+        <?php
+            }
+        ?>
+        
     </ul>
 </nav>
