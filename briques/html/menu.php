6cecd78d55eb8b25bf306c049bc29b3519a2ae30
yannrdg <yann.radigon@gmail.com>
2020-09-21 14:18:50 +0200

    Création page de déconnexion
    J'ai créé un formulaire de déconnexion.
    Pour cela j'ai utilisé la fonction session_start pour récuperer la session de l'utilisateur puis la fonction session_destroy afin de pouvoir se déconnecter.
    
    J'ai créé un lien dans le fichier menu.php afin de pouvoir se déconnecter partout.

--- briques/html/menu.php
+++ briques/html/menu.php
@@ -3,5 +3,6 @@
         <li><a href="accueil.php">Accueil</a></li>
         <li><a href="connexion.php">Connexion</a></li>
         <li><a href="inscription.php">Inscription</a></li>
+        <li><a href="deconnexion.php">Déconnexion</a></li>
     </ul>
 </nav>
