229eb3acf4acf663e5907b07d03860083e7cc526
yannrdg <yann.radigon@gmail.com>
2020-09-21 16:10:58 +0200

    Sécurisation de la page inscription
    Si l'utilisateur est connecté il ne pourra pas accéder à la page inscription. Il sera redirigé grace à un header() à la page accueil.php.

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,4 +1,9 @@
 <?php
+session_start();
+if($_SESSION['mail'])
+{
+    header('Location: accueil.php');
+}
 $title = "Inscription";
 include '../briques/html/header.php';
 include '../briques/html/menu.php';
