8ec40b3a45f12106c5fbb15f54d2a7495d6ab7c2
yannrdg <yann.radigon@gmail.com>
2020-09-28 11:38:03 +0200

    Création d'un fieldset
    Ce fieldset permet de rajouter une legend au formulaire afin de différencier le formulaire d'inscription et celui de connexion.

--- briques/html/formincription.php
+++ briques/html/formincription.php
@@ -1,6 +1,9 @@
-<form action="../pages/inscrire.php" method="post">
-    <input type="text" placeholder="Login" name="login" id="login">
-    <input type="email" placeholder="Adresse-mail" name="mail" id="mail">
-    <input type="text" placeholder="Mot de passe" name="mdp" id="mdp">
-    <input type="submit" value="Valider" name="btnValide">
-</form>
+<fieldset>
+    <legend>Inscription</legend>
+    <form action="../pages/inscrire.php" method="post">
+        <input type="text" placeholder="Login" name="login" id="login">
+        <input type="email" placeholder="Adresse-mail" name="mail" id="mail">
+        <input type="text" placeholder="Mot de passe" name="mdp" id="mdp">
+        <input type="submit" value="Valider" name="btnValide">
+    </form>
+</fieldset>
