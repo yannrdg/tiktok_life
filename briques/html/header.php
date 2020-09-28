faa9f317c5b389bdf3602a6d8ee014561d61517d
yannrdg <yann.radigon@gmail.com>
2020-09-28 11:37:48 +0200

    Modification du titre

--- briques/html/header.php
+++ briques/html/header.php
@@ -9,9 +9,10 @@
     <link rel="stylesheet" href="../briques/style/header.css">
     <link rel="stylesheet" href="../briques/style/menu.css">
     <link rel="stylesheet" href="../briques/style/footer.css">
+    <link rel="stylesheet" href="../briques/style/style.css">
     <title><?php echo $title; ?></title>
 </head>
 <body>
 <header>
-    <h1>hello</h1>
+    <h1>TikTok</h1>
 </header>
