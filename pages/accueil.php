cb6ad9412e9073f5a0c2bbcfc084a54fa6b5ec44
yannrdg <yann.radigon@gmail.com>
2020-10-14 15:57:02 +0200

    Ajout d'un main
    Afin de simplifier le code j'ai rajouté un main dans ma page accueil.php.
    
    Ce main contient l'include post.php et formPost.php qui sont les pages d'insertions des posts et d'ajout des posts.
    Le header est placé au-dessus et le footer en-dessous.

--- pages/accueil.php
+++ pages/accueil.php
@@ -9,6 +9,13 @@ else
 {
     echo "no";
 }
+?>
+<main>
+<?php
 include '../briques/html/post.php';
 include '../briques/html/formPost.php';
+?>
+</main>
+<?php
 include '../briques/html/footer.php';
+
