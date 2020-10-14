66e646c8b3342298fbc9fbb31c16cd0b108b73a8
yannrdg <yann.radigon@gmail.com>
2020-10-14 15:45:42 +0200

    Sécurisé la page inscription
    Si l'utilisateur est connecté il n'a pas besoin de s'inscrire. Il ne doit donc pas avoir accès à la page inscription sans être deconnecté.
    
    Avec un if{} je vérifie si la variable $_SESSION contient le login. Si l'utilisateur est connecté alors cette variable contient un login. Et donc l'utilisateur est redirigé directement vers la page d'accueil grâce à un header().

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,23 +1,30 @@
 <?php
 session_start();
-if($_SESSION['mail'])
+if($_SESSION['login'])
 {
     header('Location: accueil.php');
 }
-$title = "Inscription";
-include '../briques/html/header.php';
-include '../briques/html/menu.php';
-include '../briques/html/formincription.php';
-if($_GET["erreur"] == "loginNo")
+else
 {
-    echo "Ce login n'est pas disponible";
+    if($_SESSION['mail'])
+    {
+        header('Location: accueil.php');
+    }
+    $title = "Inscription";
+    include '../briques/html/header.php';
+    include '../briques/html/menu.php';
+    include '../briques/html/formincription.php';
+    if($_GET["erreur"] == "loginNo")
+    {
+        echo "Ce login n'est pas disponible";
+    }
+    if($_GET["erreur"] == "userCompte")
+    {
+        echo "Vous possédez déjà un compte !";
+    }
+    if($_GET["erreur"] == "mqueInfo")
+    {
+        echo "Il manque des informations";
+    }
+    include '../briques/html/footer.php';
 }
-if($_GET["erreur"] == "userCompte")
-{
-    echo "Vous possédez déjà un compte !";
-}
-if($_GET["erreur"] == "mqueInfo")
-{
-    echo "Il manque des informations";
-}
-include '../briques/html/footer.php';
