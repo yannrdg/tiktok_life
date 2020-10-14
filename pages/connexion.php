eaa794896d482097ab3d0e1a487ff5f1101c9011
yannrdg <yann.radigon@gmail.com>
2020-10-14 15:43:44 +0200

    Sécuriser la page connexion
    Si l'utilisateur est connecté il ne doit pas pouvoir avoir accès à la page connexion.php sans s'être déconnecté.
    
    J'ai donc avec un if{} vérifié si la variable $_SESSION contenait un login et si elle contenait un login alors cela voulait dire que l'utilisateur était connecté et par conséquent il était redirigé vers la page d'accueil.

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,18 +1,25 @@
 <?php
 session_start();
-if($_SESSION['mail'])
+if($_SESSION['login'])
 {
     header('Location: accueil.php');
 }
-$title = "Connexion";
-include '../briques/html/header.php';
-include '../briques/html/menu.php';
-include '../briques/html/formconnect.php';
-if($_GET['erreur'] == "badId")
+else
 {
-    echo "Le mot de passe ou l'identifiant n'est pas correct";
+    if($_SESSION['mail'])
+    {
+        header('Location: accueil.php');
+    }
+    $title = "Connexion";
+    include '../briques/html/header.php';
+    include '../briques/html/menu.php';
+    include '../briques/html/formconnect.php';
+    if($_GET['erreur'] == "badId")
+    {
+        echo "Le mot de passe ou l'identifiant n'est pas correct";
+    }
+    include '../briques/html/footer.php';
 }
-include '../briques/html/footer.php';
 
 
    
