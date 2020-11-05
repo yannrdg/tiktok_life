d120d7a5aca4dcf38c316434462e57961155af1d
yannrdg <yann.radigon@gmail.com>
2020-11-05 17:45:28 +0100

    Ajout d'un main
    J'ai décidé de mettre mes formulaires de connexion et d'inscription dans une balise main afin de pouvoir mieux les gérer en css et d'avoir un code html plus propre et plus lisible.

--- briques/html/formconnect.php
+++ briques/html/formconnect.php
@@ -1,8 +1,19 @@
-<fieldset>
-    <legend>Connexion</legend>
-    <form action="../pages/connecter.php" method="post">
-        <input type="text" name="identifiant" placeholder="identifiant">
-        <input type="password" name="mdpConnect" placeholder="Votre mot de passe">
-        <input type="submit" name="btnConnect" value="Se connecter">
-    </form>
-</fieldset>
+<main id="connexion">
+    <fieldset>
+        <legend>Connexion</legend>
+        <form action="../pages/connecter.php" method="post">
+            <input type="text" name="identifiant" placeholder="Identifiant">
+            <input type="password" name="mdpConnect" placeholder="Votre mot de passe">
+            <input type="submit" name="btnConnect" value="Se connecter">
+        </form>
+        <p>
+        <?php
+            if($_GET['erreur'] == "badId")
+            {
+                echo "Le mot de passe ou l'identifiant n'est pas correct";
+            }
+        ?>
+        </p>
+    </fieldset>
+    <a href="inscription.php">Première inscription</a>
+</main>
