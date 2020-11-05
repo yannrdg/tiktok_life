8c0ccfa5e1a43fa8ab975efec24a18205eac4697
yannrdg <yann.radigon@gmail.com>
2020-11-05 18:04:03 +0100

    Modification du type de l'input
    J'ai changer le type de l'input de text en password afin de pouvoir cacher le mot de passe lorsque l'utilisateur le saisie.

--- briques/html/formincription.php
+++ briques/html/formincription.php
@@ -4,7 +4,7 @@
         <form action="../pages/inscrire.php" method="post">
             <input type="text" placeholder="Login" name="login" id="login">
             <input type="email" placeholder="Adresse-mail" name="mail" id="mail">
-            <input type="text" placeholder="Mot de passe" name="mdp" id="mdp">
+            <input type="password" placeholder="Mot de passe" name="mdp" id="mdp">
             <input type="submit" value="Valider" name="btnValide">
         </form>
         <p>
