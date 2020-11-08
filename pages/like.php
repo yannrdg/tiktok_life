562f93a695912cdd7d5c60a748b94c7afff3af13
yannrdg <yann.radigon@gmail.com>
2020-11-08 16:54:04 +0100

    Gestion des likes
    Lorsque j'appuie sur le coeur je me redirige vers le fichier like.php avec une balise <a>.
    
    A l'aide de la méthode GET j'ajoute dans l'url l'idPost et le login de l'utilisateur.
    
    Dans le fichier like.php je récupère ces 2 informations avec la méthode $_GET.
    Je fais ensuite une requète SQL SELECT afin de rechercher le nombre de résultat pour cet idPost et cet utilisateur à l'aide de la fonction rowCount().
    
    Si le résultat renvoie 0, à l'aide d'une requète SQL INSERT INTO j'ajoute dans ma table Jaime une ligne avec idPost = idPost et user = le login de l'utilisateur. Le like est donc ajouté.
    
    Si le résultat renvoie 1 c'est que le like est déjà présent. Donc si je reclique c'est que je souhaite l'enlevé. Je procède donc avec une requète SQL DELET afin de supprimer la ligne du like.

--- pages/like.php
+++ pages/like.php
@@ -1,2 +1,34 @@
 <?php
-echo "ok";
+try{
+    include 'config.php';
+    $bdd  = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+
+    $idPost = $_GET['id'];
+    $login = $_GET['login'];
+    $reqLike = $bdd->prepare('SELECT * FROM Jaime WHERE idPost = :idPost && user = :user');
+    $reqLike->bindParam(':idPost', $idPost);
+    $reqLike->bindParam(':user', $login);
+    $reqLike->execute();
+    $allLike = $reqLike->rowCount();
+
+    if($allLike == 0)
+    {
+        $ajoutLike = $bdd->prepare('INSERT INTO Jaime (idPost, user) VALUES (:idPost, :user)');
+        $ajoutLike->bindParam(':idPost', $idPost);
+        $ajoutLike->bindParam(':user', $login);
+        $ajoutLike->execute();
+        header('Location: accueil.php');
+    }
+    else if($allLike == 1)
+    {
+        $supLike = $bdd->prepare('DELETE FROM Jaime WHERE idPost = :idPost && user = :user');
+        $supLike->bindParam(':idPost', $idPost);
+        $supLike->bindParam(':user', $login);
+        $supLike->execute();
+        header('Location: accueil.php');
+    }
+} 
+catch(PPDOException $Exception)
+{
+    echo 'Impossible de traiter les données. Erreur : '.$Exception->getMessage();
+}
