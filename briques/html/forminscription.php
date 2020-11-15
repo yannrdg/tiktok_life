44bf906bee243897ab54182b43e3f0815fe35a3c
yannrdg <yann.radigon@gmail.com>
2020-11-15 19:14:58 +0100

    Affichage des messages d'erreur
    Les messages d'erreur étaient affichés avec la méthode GET en passant par l'URL.
    
    Comme le fichier est ajouté à l'aide d'un require dans le fichier inscription.php je peux afficher les messages d'erreur différemment.
    
    Pour cela j'ai créé une variable $errorMsg qui contient les différents messages d'erreur en fonction de l'erreur.
    Puis je l'affiche dans une balise <p> à l'aide d'un echo.
    Le message est affiché si la variable $errorMsg n'est pas vide.
    
    Pour traiter cette condition j'ai utilisé un if() et la condition est !empty($errorMsg) c'est à dire que la variable n'est pas vide.

--- briques/html/forminscription.php
+++ briques/html/forminscription.php
@@ -1,27 +1,19 @@
 <main id="inscription">
     <fieldset>
         <legend>Inscription</legend>
-        <form action="../pages/inscrire.php" method="post">
+        <form action="" method="post">
             <input type="text" placeholder="Login" name="login" id="login">
             <input type="email" placeholder="Adresse-mail" name="mail" id="mail">
             <input type="password" placeholder="Mot de passe" name="mdp" id="mdp">
             <input type="submit" value="Valider" name="btnValide">
         </form>
-        <p>
         <?php
-             if($_GET["erreur"] == "loginNo")
-             {
-                 echo "Ce login n'est pas disponible";
-             }
-             if($_GET["erreur"] == "userCompte")
-             {
-                 echo "Vous possédez déjà un compte !";
-             }
-             if($_GET["erreur"] == "mqueInfo")
-             {
-                 echo "Il manque des informations";
-             }
+        if(!empty($errorMsg))
+        {
         ?>
-        </p>
+        <p><?php echo $errorMsg; ?></p>
+        <?php
+        }
+        ?>
     </fieldset>
 </main>
