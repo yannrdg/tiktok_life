8dcc01a4eee561445a8d4930bcbb775fa600f210
yannrdg <yann.radigon@gmail.com>
2020-11-15 19:25:11 +0100

    Gestion du fichier inscription.php
    Afin de pouvoir gérer le fichier en POO et d'avoir une meilleure organisation j'ai modifié le fichier inscription.php.
    
    Tout d'abord je récupère grâce à un require la class Utilisateur.
    
    Si l'utilisateur appuie sur le bouton d'envoi du formulaire alors il faut réaliser l'action demandée.
    
    Tout d'abord on stocke les valeurs des champs renseignés dans des variables.
    On vérifie alors si tous les champs on bien été renseignés et qu'ils ne sont pas vides d'où le !empty().
    
    Si tous les champs sont complétés alors on se connecte à la bdd à l'aide d'un new PDO afin de pouvoir vérifier des informations.
    
    On crée un objet de type Utilisateur. On crée alors une instance de la classe Utilisateur. On va alors pouvoir lui ajouter ses différents attributs qui correpsondent aux informations que l'utilisateir à rentré dans les différents champs.
    On va allors pouvoir invoquer les différentes méthodes(fonctions) à l'objet.
    
    Pour les méthodes LoginExist() et EmailExist() on va vérifier leur valeur. Si elles retournent true c'est à dire que le résultat est différent (!=) de 0 (donc = 1), il y a donc un login ou un mail identique. Un message d'erreur est donc envoyé. Si par contre ces méthodes renvoient false (donc = 0, d'où le !LoginExist) alors on peut continuer.
    
    On va alors invoquer la méthode EnregistreInfoBase à l'objet $utilisateur qui va envoyer les informations à la base de données.
    
    Si aucun message d'erreur (C'est à dire que la variable $errorMsg est toujours vide) alors on peut être redirigé vers la page de connexion.

--- pages/inscription.php
+++ pages/inscription.php
@@ -1,17 +1,58 @@
 <?php
 session_start();
-if($_SESSION['login'])
+require 'config.php';
+require '../donnees/Utilisateur.php';
+
+//Faire l'action demandée (Inscrire si le formulaire est envoyé)
+if(isset($_POST['btnValide']))
 {
-    header('Location: accueil.php');
-}
-else
-{
-    if($_SESSION['mail'])
+    $login = htmlspecialchars($_POST['login']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $mdp = password_hash($_POST['mdp'],  PASSWORD_DEFAULT);
+    if(!empty($login) && !empty($mail) && !empty($mdp))
     {
-        header('Location: accueil.php');
+        try
+        {
+            $bdd = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
+	    
+            $utilisateur = new Utilisateur($bdd, $mail, $login, $mdp); 
+            
+            //Recherche si le mail est déjà existant
+            if(!$utilisateur->EmailExist())
+            {
+                //Recherche si le login est déjà existant
+                if(!$utilisateur->LoginExist())
+                {
+		             $utilisateur->EnregistrerInfoBase();
+                }
+                else
+                {
+                    $errorMsg = "Le login est déjà pris";
+                }
+            }
+            else
+            {
+                $errorMsg = "Vous possèdez déjà un compte avec ce mail";
+            }
+        }
+        catch(PDOException $Exception)
+        {
+            $errorMsg = "Votre demande n'a pas pu être traitée. Raison: " . $Exception->getMessage();
+        }
     }
-    $title = "Inscription";
-    include '../briques/html/header.php';
-    include '../briques/html/menu.php';
-    include '../briques/html/formincription.php';
+    else
+    {
+       $errorMsg = "Il manque des informations";
+    }
+    if(empty($errorMsg))
+    {
+        header('Location: connexion.php');
+    }
 }
+
+// Afficher la page correspondante 
+require '../briques/html/header.php';
+require '../briques/html/forminscription.php';
+require '../briques/html/footer.php';
+
+//Sinon rediriger vers une autre page
